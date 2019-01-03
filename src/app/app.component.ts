c910d141c686c2ca6a40f89c302c18ea4d242f50
Zouhair Khallaf <zouhairkhallaf@gmail.com>
2019-01-02 21:05:55 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h1 {
+      color: green;
+    }
+  `]
 })
+
 export class AppComponent {
-  title = 'my-first-angular-app';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// this first code that gets executed 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
